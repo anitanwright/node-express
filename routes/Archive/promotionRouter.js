ecef633bf5b67a228fb850aa05c7207a59cb31fb
Anita Wright <anitanwright@gmail.com>
2021-10-20 23:58:23 -0700

    update to routes

---
+++ routes/Archive/promotionRouter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const promotionRouter = express.Router();
+
+// single statement that handles all the routes to promotion endpoints
+promotionRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the promotions to you');
+})
+.post((req, res) => {
+    res.end(`Will add the promotions: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res) => {
+    res.end('Deleting all promotions');
+})
+
+//single statement with endpoints for specific promotions with promotionID params
+
+promotionRouter.route('/:promotionId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send the details of the promotion: ${req.params.promotionId} to you`);
+})
+.post((req, res) => {
+    res.end(`Will add the promotions: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /promotions/${req.params.promotionId}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting promotion: ${req.params.promotionId}`);
+});
+
+module.exports = promotionRouter;
